81fd755082e54e4418fc247c937c72368d9f7487
hzk <huangzike@qixiaowei.net>
2022-09-24 11:57:53 +0800

    add:dbStatus

---
+++ qixiaowei-integration/qixiaowei-integration-common/src/main/java/net/qixiaowei/integration/common/constant/DBStatusConstants.java
@@ -0,0 +1,17 @@
+package net.qixiaowei.integration.common.constant;
+
+/**
+ * 数据状态常量信息
+ */
+public interface DBStatusConstants {
+
+    /**
+     * 正常
+     */
+    int NORMAL = 1;
+    /**
+     * 删除
+     */
+    int DELETE = -1;
+
+}
