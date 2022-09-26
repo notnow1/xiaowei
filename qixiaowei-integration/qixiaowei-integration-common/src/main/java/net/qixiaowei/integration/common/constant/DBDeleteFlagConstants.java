4c28138c0195e8d40710426c1fcdca1401d65ab1
汤密持 <tangmichi@qixiaowei.net>
2022-09-26 16:18:21 +0800

    [项目名称：企小微][提交人：汤密持][提交说明：修改数据库数据状态枚举]

---
+++ qixiaowei-integration/qixiaowei-integration-common/src/main/java/net/qixiaowei/integration/common/constant/DBDeleteFlagConstants.java
@@ -0,0 +1,16 @@
+package net.qixiaowei.integration.common.constant;
+
+/**
+ * 数据状态常量信息
+ */
+public interface DBDeleteFlagConstants {
+    /**
+     * 数据正常状态
+     */
+    int DELETE_FLAG_ZERO = 0;
+    /**
+     * 数据逻辑删除状态
+     */
+    int DELETE_FLAG_ONE  = 1;
+
+}
