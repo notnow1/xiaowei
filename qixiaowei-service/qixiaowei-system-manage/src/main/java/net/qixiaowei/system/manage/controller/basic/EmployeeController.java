ba68cba9abbba3b834276e4b7595bad98fa9b698
汤密持 <tangmichi@qixiaowei.net>
2022-10-18 14:48:27 +0800

    [项目名称：企小微租户系统][提交人：汤密持][提交说明：回滚]

--- qixiaowei-service/qixiaowei-system-manage/src/main/java/net/qixiaowei/system/manage/controller/basic/EmployeeController.java
+++ qixiaowei-service/qixiaowei-system-manage/src/main/java/net/qixiaowei/system/manage/controller/basic/EmployeeController.java
@@ -1,36 +1,25 @@
 package net.qixiaowei.system.manage.controller.basic;
 
-import java.io.BufferedInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URLEncoder;
-import java.util.List;
-import java.util.Map;
-
 import com.alibaba.excel.EasyExcel;
 import com.alibaba.excel.read.builder.ExcelReaderBuilder;
-import io.swagger.annotations.ApiOperation;
-import lombok.SneakyThrows;
-import org.apache.commons.codec.Charsets;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.StringUtils;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import net.qixiaowei.integration.common.web.page.TableDataInfo;
-import net.qixiaowei.integration.common.web.domain.AjaxResult;
-import net.qixiaowei.integration.log.annotation.Log;
-import org.springframework.stereotype.Controller;
-import net.qixiaowei.integration.log.enums.BusinessType;
-import net.qixiaowei.system.manage.api.dto.basic.EmployeeDTO;
-import net.qixiaowei.system.manage.service.basic.IEmployeeService;
-import net.qixiaowei.integration.security.annotation.RequiresPermissions;
+import net.qixiaowei.integration.common.exception.ServiceException;
+import net.qixiaowei.integration.common.utils.StringUtils;
 import net.qixiaowei.integration.common.web.controller.BaseController;
+import net.qixiaowei.integration.common.web.domain.AjaxResult;
+import net.qixiaowei.integration.common.web.page.TableDataInfo;
+import net.qixiaowei.system.manage.api.dto.basic.EmployeeDTO;
+import net.qixiaowei.system.manage.excel.EmployeeExcel;
+import net.qixiaowei.system.manage.excel.EmployeeImportListener;
+import net.qixiaowei.system.manage.service.basic.IEmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
 
 
 /**
@@ -48,50 +37,7 @@ public class EmployeeController extends BaseController
     private IEmployeeService employeeService;
 
 
-    /**
-     * 导入用户
-     */
-    @PostMapping("import-user")
-    public AjaxResult importUser(MultipartFile file, Integer isCovered) {
-        String filename = file.getOriginalFilename();
-        if (StringUtils.isEmpty(filename)) {
-            throw new RuntimeException("请上传文件!");
-        }
-        if ((!StringUtils.endsWithIgnoreCase(filename, ".xls") && !StringUtils.endsWithIgnoreCase(filename, ".xlsx"))) {
-            throw new RuntimeException("请上传正确的excel文件!");
-        }
-        InputStream inputStream;
-        try {
 
-            inputStream = new BufferedInputStream(file.getInputStream());
-            ExcelReaderBuilder builder = EasyExcel.read(inputStream, UserExcel.class, importListener);
-            builder.doReadAll();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return AjaxResult.success("操作成功");
-    }
-
-    /**
-     * 导出用户
-     */
-    @SneakyThrows
-    @GetMapping("export-user")
-    @ApiOperationSupport(order = 13)
-    @ApiOperation(value = "导出用户", notes = "传入user")
-    public void exportUser(@ApiIgnore @RequestParam Map<String, Object> user, BladeUser bladeUser, HttpServletResponse response) {
-        QueryWrapper<User> queryWrapper = Condition.getQueryWrapper(user, User.class);
-        if (!SecureUtil.isAdministrator()){
-            queryWrapper.lambda().eq(User::getTenantId, bladeUser.getTenantId());
-        }
-        queryWrapper.lambda().eq(User::getIsDeleted, BladeConstant.DB_NOT_DELETED);
-        List<UserExcel> list = userService.exportUser(queryWrapper);
-        response.setContentType("application/vnd.ms-excel");
-        response.setCharacterEncoding(Charsets.UTF_8.name());
-        String fileName = URLEncoder.encode("用户数据导出", Charsets.UTF_8.name());
-        response.setHeader("Content-disposition", "attachment;filename=" + fileName + ".xlsx");
-        EasyExcel.write(response.getOutputStream(), UserExcel.class).sheet("用户数据表").doWrite(list);
-    }
 
     /**
     * 分页查询员工表列表
