3e7af478aad3d67bd7f66d0ad3f108d49a94690a
汤密持 <tangmichi@qixiaowei.net>
2022-12-16 20:11:33 +0800

    [项目名称：企小微租户系统][提交人：汤密持][提交说明：奖金发放台账]

---
+++ qixiaowei-service-api/qixiaowei-operate-cloud-api/src/main/java/net/qixiaowei/operate/cloud/api/dto/bonus/BonusPayStandingDTO.java
@@ -0,0 +1,122 @@
+package net.qixiaowei.operate.cloud.api.dto.bonus;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import javax.validation.groups.Default;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+
+/**
+* 奖金发放台账
+* @author TANGMICHI
+* @since 2022-12-08
+*/
+@Data
+@Accessors(chain = true)
+public class BonusPayStandingDTO {
+
+    //查询检验
+    public interface QueryBonusPayStandingDTO extends Default{
+
+    }
+    //新增检验
+    public interface AddBonusPayStandingDTO extends Default{
+
+    }
+
+    //删除检验
+    public interface DeleteBonusPayStandingDTO extends Default{
+
+    }
+    //修改检验
+    public interface UpdateBonusPayStandingDTO extends Default{
+
+    }
+
+
+    /**
+     * 部门ID
+     */
+    private  Long  departmentId;
+    /**
+     * 部门名称
+     */
+    private  String  departmentName;
+    /**
+     * 奖金发放台账工资项集合
+     */
+    private List<BonusPaySalaryDTO> bonusPaySalaryDTOs;
+    /**
+     * 年初预算包
+     */
+    private BigDecimal beYearAmountBonusBudget;
+    /**
+     * 获奖时间-年月
+     */
+    private  Integer awardYearAndMonth;
+    /**
+     * 获奖时间-年
+     */
+    private  Integer awardYear;
+    /**
+     * 获奖时间-月
+     */
+    private  Integer awardMonth;
+
+    /**
+     * 一月实际值
+     */
+    private BigDecimal amountJanuary;
+    /**
+     * 二月实际值
+     */
+    private BigDecimal amountFebruary;
+    /**
+     * 三月实际值
+     */
+    private BigDecimal amountMarch;
+    /**
+     * 四月实际值
+     */
+    private BigDecimal amountApril;
+    /**
+     * 五月实际值
+     */
+    private BigDecimal amountMay;
+    /**
+     * 六月实际值
+     */
+    private BigDecimal amountJune;
+    /**
+     * 七月实际值
+     */
+    private BigDecimal amountJuly;
+    /**
+     * 八月实际值
+     */
+    private BigDecimal amountAugust;
+    /**
+     * 九月实际值
+     */
+    private BigDecimal amountSeptember;
+    /**
+     * 十月实际值
+     */
+    private BigDecimal amountOctober;
+    /**
+     * 十一月实际值
+     */
+    private BigDecimal amountNovember;
+    /**
+     * 十二月实际值
+     */
+    private BigDecimal amountDecember;
+
+
+
+
+}
+
