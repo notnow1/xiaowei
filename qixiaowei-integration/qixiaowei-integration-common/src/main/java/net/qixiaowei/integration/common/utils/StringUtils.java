1c295c6ce51025a98412d7f10abfa3275610101e
hzk <huangzike@qixiaowei.net>
2023-01-17 18:31:49 +0800

    1.dependencies update 2.bug fix

--- qixiaowei-integration/qixiaowei-integration-common/src/main/java/net/qixiaowei/integration/common/utils/StringUtils.java
+++ qixiaowei-integration/qixiaowei-integration-common/src/main/java/net/qixiaowei/integration/common/utils/StringUtils.java
@@ -10,8 +10,6 @@ import java.util.Map;
 
 /**
  * 字符串工具类
- *
- * 
  */
 public class StringUtils extends org.apache.commons.lang3.StringUtils {
     /**
@@ -378,6 +376,9 @@ public class StringUtils extends org.apache.commons.lang3.StringUtils {
     public static String toCamelCase(String s) {
         if (s == null) {
             return null;
+        }
+        if (s.indexOf(SEPARATOR) == -1) {
+            return s;
         }
         s = s.toLowerCase();
         StringBuilder sb = new StringBuilder(s.length());
