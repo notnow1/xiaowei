6e7f650af55bdf18719044aa9c4ce0c8921b96a8
汤密持 <tangmichi@qixiaowei.net>
2023-03-03 11:48:01 +0800

    [项目名称：战略云微服务][提交人：汤密持][提交说明：预制sql]

--- sql/init/dml/system_manage_business-dml.sql
+++ sql/init/dml/system_manage_business-dml.sql
@@ -71,18 +71,19 @@ INSERT INTO `indicator` (`indicator_id`, `parent_indicator_id`, `ancestors`, `in
 -- ----------------------------
 -- init of dictionary_type
 -- ----------------------------
-INSERT INTO dictionary_type (dictionary_type_id, dictionary_type, dictionary_name, menu_zeroth_name, menu_first_name, menu_second_name, remark, status, delete_flag, create_by, create_time, update_by, update_time) VALUES(1, 'PRODUCT_CATEGORY', '产品类别', '设置管理', '经营云配置', '产品配置', '', 1, 0, 0, sysdate(), 0, sysdate());
+INSERT INTO dictionary_type (dictionary_type, dictionary_name, menu_zeroth_name, menu_first_name, menu_second_name, remark, status, delete_flag, create_by, create_time, update_by, update_time,tenant_id) VALUES('PRODUCT_CATEGORY', '产品类别', '设置管理', '经营云配置', '产品配置', '', 1, 0, 0, sysdate(), 0, sysdate(),0);
 
 
 -- ----------------------------
 -- init of dictionary_data
 -- ----------------------------
-INSERT INTO dictionary_data (dictionary_data_id, dictionary_type_id, dictionary_label, dictionary_value, default_flag, sort, remark, status, delete_flag, create_by, create_time, update_by, update_time) VALUES(1, 1, '通用件', '1', 1, 1, '', 1, 0, 1, sysdate(), 1, sysdate());
-INSERT INTO dictionary_data (dictionary_data_id, dictionary_type_id, dictionary_label, dictionary_value, default_flag, sort, remark, status, delete_flag, create_by, create_time, update_by, update_time) VALUES(2, 1, '标准件', '2', 0, 2, '', 1, 0, 1, sysdate(), 1, sysdate());
-INSERT INTO dictionary_data (dictionary_data_id, dictionary_type_id, dictionary_label, dictionary_value, default_flag, sort, remark, status, delete_flag, create_by, create_time, update_by, update_time) VALUES(3, 1, '自制件', '3', 0, 3, '', 1, 0, 1, sysdate(), 1, sysdate());
-INSERT INTO dictionary_data (dictionary_data_id, dictionary_type_id, dictionary_label, dictionary_value, default_flag, sort, remark, status, delete_flag, create_by, create_time, update_by, update_time) VALUES(4, 1, '外购件', '4', 0, 4, '', 1, 0, 1, sysdate(), 1, sysdate());
-INSERT INTO dictionary_data (dictionary_data_id, dictionary_type_id, dictionary_label, dictionary_value, default_flag, sort, remark, status, delete_flag, create_by, create_time, update_by, update_time) VALUES(5, 1, '外协件', '5', 0, 5, '', 1, 0, 1, sysdate(), 1, sysdate());
-INSERT INTO dictionary_data (dictionary_data_id, dictionary_type_id, dictionary_label, dictionary_value, default_flag, sort, remark, status, delete_flag, create_by, create_time, update_by, update_time) VALUES(6, 1, '原材料', '6', 0, 6, '', 1, 0, 1, sysdate(), 1, sysdate());
+Set @dictionary_type_id = (select dictionary_type_id  from dictionary_type where  dictionary_type='PRODUCT_CATEGORY' and tenant_id =0);
+INSERT INTO dictionary_data ( dictionary_type_id, dictionary_label, dictionary_value, default_flag, sort, remark, status, delete_flag, create_by, create_time, update_by, update_time) VALUES( @dictionary_type_id, '通用件', '1', 1, 1, '', 1, 0, 1, sysdate(), 1, sysdate());
+INSERT INTO dictionary_data ( dictionary_type_id, dictionary_label, dictionary_value, default_flag, sort, remark, status, delete_flag, create_by, create_time, update_by, update_time) VALUES( @dictionary_type_id, '标准件', '2', 0, 2, '', 1, 0, 1, sysdate(), 1, sysdate());
+INSERT INTO dictionary_data (dictionary_type_id, dictionary_label, dictionary_value, default_flag, sort, remark, status, delete_flag, create_by, create_time, update_by, update_time) VALUES( @dictionary_type_id, '自制件', '3', 0, 3, '', 1, 0, 1, sysdate(), 1, sysdate());
+INSERT INTO dictionary_data (dictionary_type_id, dictionary_label, dictionary_value, default_flag, sort, remark, status, delete_flag, create_by, create_time, update_by, update_time) VALUES( @dictionary_type_id, '外购件', '4', 0, 4, '', 1, 0, 1, sysdate(), 1, sysdate());
+INSERT INTO dictionary_data (dictionary_type_id, dictionary_label, dictionary_value, default_flag, sort, remark, status, delete_flag, create_by, create_time, update_by, update_time) VALUES( @dictionary_type_id, '外协件', '5', 0, 5, '', 1, 0, 1, sysdate(), 1, sysdate());
+INSERT INTO dictionary_data (dictionary_type_id, dictionary_label, dictionary_value, default_flag, sort, remark, status, delete_flag, create_by, create_time, update_by, update_time) VALUES( @dictionary_type_id, '原材料', '6', 0, 6, '', 1, 0, 1, sysdate(), 1, sysdate());
 
 
 -- ----------------------------
