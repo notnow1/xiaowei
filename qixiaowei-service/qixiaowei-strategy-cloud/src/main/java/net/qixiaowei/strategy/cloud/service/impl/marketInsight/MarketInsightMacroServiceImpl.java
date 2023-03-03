a61066dfe02cde06c1b4d807ae151f01a0f26d47
汤密持 <tangmichi@qixiaowei.net>
2023-03-03 11:23:32 +0800

    [项目名称：战略云微服务][提交人：汤密持][提交说明：代码规范]

--- qixiaowei-service/qixiaowei-strategy-cloud/src/main/java/net/qixiaowei/strategy/cloud/service/impl/marketInsight/MarketInsightMacroServiceImpl.java
+++ qixiaowei-service/qixiaowei-strategy-cloud/src/main/java/net/qixiaowei/strategy/cloud/service/impl/marketInsight/MarketInsightMacroServiceImpl.java
@@ -321,6 +321,10 @@ public class MarketInsightMacroServiceImpl implements IMarketInsightMacroService
         BeanUtils.copyProperties(marketInsightMacroDTO, marketInsightMacro);
         marketInsightMacro.setUpdateTime(DateUtils.getNowDate());
         marketInsightMacro.setUpdateBy(SecurityUtils.getUserId());
+        List<MarketInsightMacroDTO> marketInsightMacroDTOS = marketInsightMacroMapper.selectMarketInsightMacroList(marketInsightMacro);
+        if (StringUtils.isNotEmpty(marketInsightMacroDTOS)){
+            throw new ServiceException("已存在该年份和规划业务单元数据！请重新修改！");
+        }
         try {
             i = marketInsightMacroMapper.updateMarketInsightMacro(marketInsightMacro);
         } catch (Exception e) {
