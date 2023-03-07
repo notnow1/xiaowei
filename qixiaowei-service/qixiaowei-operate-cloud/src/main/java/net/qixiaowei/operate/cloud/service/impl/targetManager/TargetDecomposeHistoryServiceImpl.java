c8b12a9c313571561a672a205c546646683df7a0
汤密持 <tangmichi@qixiaowei.net>
2023-03-07 14:24:29 +0800

    [项目名称：bug修复微服务][提交人：汤密持][提交说明：修复目标完成率计算]

--- qixiaowei-service/qixiaowei-operate-cloud/src/main/java/net/qixiaowei/operate/cloud/service/impl/targetManager/TargetDecomposeHistoryServiceImpl.java
+++ qixiaowei-service/qixiaowei-operate-cloud/src/main/java/net/qixiaowei/operate/cloud/service/impl/targetManager/TargetDecomposeHistoryServiceImpl.java
@@ -142,11 +142,11 @@ public class TargetDecomposeHistoryServiceImpl implements ITargetDecomposeHistor
                     }
 
                 }
-                if (null != targetPercentageComplete && targetPercentageComplete.compareTo(BigDecimal.ZERO) != 0) {
+                if (actualTotal.compareTo(BigDecimal.ZERO) != 0) {
                     //被除数 不能为0和空
                     if (null != decomposeDetailsSnapshotDTO.getDecomposeTarget() && decomposeDetailsSnapshotDTO.getDecomposeTarget().compareTo(BigDecimal.ZERO) != 0) {
                         //保留一位小数
-                        targetPercentageComplete = targetPercentageComplete.divide(decomposeDetailsSnapshotDTO.getDecomposeTarget(), BigDecimal.ROUND_HALF_UP);
+                        targetPercentageComplete = actualTotal.divide(decomposeDetailsSnapshotDTO.getDecomposeTarget(), BigDecimal.ROUND_HALF_UP);
                     }
                 }
                 decomposeDetailsSnapshotDTO.setForecastYear(forecastYear);
