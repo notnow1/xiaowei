88d721a19241187845511480a4eab281e4308657
chen.wang <wangchen@qixiaowei.net>
2023-03-07 15:06:41 +0800

    [项目名称：企小微租户系统][提交人:王晨][战略枚举更新]

--- qixiaowei-integration/qixiaowei-integration-common/src/main/java/net/qixiaowei/integration/common/enums/message/BusinessType.java
+++ qixiaowei-integration/qixiaowei-integration-common/src/main/java/net/qixiaowei/integration/common/enums/message/BusinessType.java
@@ -65,6 +65,8 @@ public enum BusinessType {
     MARKET_INSIGHT_OPPONENT(56, "市场洞察对手"),
     MARKET_INSIGHT_SELF(57, "市场洞察自身"),
     STRATEGY_INDEX_DIMENSION(58, "战略指标维度"),
+    STRATEGY_MEASURE(59, "战略举措清单"),
+    STRATEGY_METRICS(60, "战略衡量指标"),
     ;
 
 
