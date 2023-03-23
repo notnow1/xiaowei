aa916d92c8696ddb1387adbfc8cce4750c7e291b
汤密持 <tangmichi@qixiaowei.net>
2023-03-23 19:08:23 +0800

    [项目名称：战略云微服务][提交人：汤密持][提交说明：列表字段排序]

--- qixiaowei-service/qixiaowei-strategy-cloud/src/main/java/net/qixiaowei/strategy/cloud/logic/impl/field/list/MarketInsightSelfFieldListConfigImpl.java
+++ qixiaowei-service/qixiaowei-strategy-cloud/src/main/java/net/qixiaowei/strategy/cloud/logic/impl/field/list/MarketInsightSelfFieldListConfigImpl.java
@@ -31,9 +31,9 @@ public class MarketInsightSelfFieldListConfigImpl implements IFieldListConfigStr
         INIT_MAP.put(MarketInsightSelfField.PLAN_YEAR.getCode(), FieldListConfig.builder().fieldWidth(120).sort(1).showFlag(1).fixationFlag(0).showForce(0).fixationForce(0).build());
         INIT_MAP.put(MarketInsightSelfField.PLAN_BUSINESS_UNIT_ID.getCode(), FieldListConfig.builder().fieldWidth(160).sort(2).showFlag(1).fixationFlag(0).showForce(0).fixationForce(0).build());
         INIT_MAP.put(MarketInsightSelfField.AREA_ID.getCode(), FieldListConfig.builder().fieldWidth(130).sort(3).showFlag(1).fixationFlag(0).showForce(0).fixationForce(0).build());
-        INIT_MAP.put(MarketInsightSelfField.DEPARTMENT_ID.getCode(), FieldListConfig.builder().fieldWidth(130).sort(3).showFlag(1).fixationFlag(0).showForce(0).fixationForce(0).build());
-        INIT_MAP.put(MarketInsightSelfField.PRODUCT_ID.getCode(), FieldListConfig.builder().fieldWidth(130).sort(3).showFlag(1).fixationFlag(0).showForce(0).fixationForce(0).build());
-        INIT_MAP.put(MarketInsightSelfField.INDUSTRY_ID.getCode(), FieldListConfig.builder().fieldWidth(130).sort(3).showFlag(1).fixationFlag(0).showForce(0).fixationForce(0).build());
+        INIT_MAP.put(MarketInsightSelfField.DEPARTMENT_ID.getCode(), FieldListConfig.builder().fieldWidth(130).sort(4).showFlag(1).fixationFlag(0).showForce(0).fixationForce(0).build());
+        INIT_MAP.put(MarketInsightSelfField.PRODUCT_ID.getCode(), FieldListConfig.builder().fieldWidth(130).sort(5).showFlag(1).fixationFlag(0).showForce(0).fixationForce(0).build());
+        INIT_MAP.put(MarketInsightSelfField.INDUSTRY_ID.getCode(), FieldListConfig.builder().fieldWidth(130).sort(6).showFlag(1).fixationFlag(0).showForce(0).fixationForce(0).build());
     }
 
     @Override
