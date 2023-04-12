6d6dbce9378c0627f374bddd3d7c2308add117fe
chen.wang <wangchen@qixiaowei.net>
2023-04-12 18:51:31 +0800

    [项目名称：企小微租户系统][提交人:王晨][指标行业树结构]

--- qixiaowei-service/qixiaowei-system-manage/src/main/java/net/qixiaowei/system/manage/service/impl/basic/IndicatorServiceImpl.java
+++ qixiaowei-service/qixiaowei-system-manage/src/main/java/net/qixiaowei/system/manage/service/impl/basic/IndicatorServiceImpl.java
@@ -10,7 +10,6 @@ import net.qixiaowei.integration.common.domain.R;
 import net.qixiaowei.integration.common.enums.PrefixCodeRule;
 import net.qixiaowei.integration.common.enums.basic.IndicatorCode;
 import net.qixiaowei.integration.common.exception.ServiceException;
-import net.qixiaowei.integration.common.utils.CheckObjectIsNullUtils;
 import net.qixiaowei.integration.common.utils.DateUtils;
 import net.qixiaowei.integration.common.utils.StringUtils;
 import net.qixiaowei.integration.common.utils.bean.BeanUtils;
@@ -29,7 +28,6 @@ import net.qixiaowei.operate.cloud.api.remote.targetManager.RemoteSettingService
 import net.qixiaowei.strategy.cloud.api.dto.gap.GapAnalysisDTO;
 import net.qixiaowei.strategy.cloud.api.dto.gap.GapAnalysisOperateDTO;
 import net.qixiaowei.strategy.cloud.api.dto.marketInsight.MiOpponentFinanceDTO;
-import net.qixiaowei.strategy.cloud.api.dto.strategyDecode.AnnualKeyWorkDetailDTO;
 import net.qixiaowei.strategy.cloud.api.dto.strategyDecode.StrategyMetricsDTO;
 import net.qixiaowei.strategy.cloud.api.dto.strategyDecode.StrategyMetricsDetailDTO;
 import net.qixiaowei.strategy.cloud.api.remote.gap.RemoteGapAnalysisService;
@@ -418,17 +416,14 @@ public class IndicatorServiceImpl implements IIndicatorService {
         if (StringUtils.isNotNull(indicatorDTO.getParentIndicatorId())) {
             parentIndicatorId = indicatorDTO.getParentIndicatorId();
             IndicatorDTO parentIndicator = indicatorMapper.selectIndicatorByIndicatorId(parentIndicatorId);
-            if (parentIndicator == null && !parentIndicatorId.equals(0L)) {
+            if (StringUtils.isNull(parentIndicator) && !parentIndicatorId.equals(0L)) {
                 throw new ServiceException("上级指标不存在");
             }
-            if (!indicatorById.getParentIndicatorId().equals(parentIndicatorId) && !parentIndicatorId.equals(0L)) {
-                // 路径修改
-                ancestors = parentIndicator.getAncestors();
-                if (StringUtils.isNotEmpty(ancestors)) {
-                    ancestors = ancestors + ",";
-                }
-                ancestors = ancestors + parentIndicatorId;
+            ancestors = parentIndicator.getAncestors();
+            if (StringUtils.isNotEmpty(ancestors)) {
+                ancestors = ancestors + ",";
             }
+            ancestors = ancestors + parentIndicatorId;
         }
         Indicator indicator = new Indicator();
         BeanUtils.copyProperties(indicatorDTO, indicator);
