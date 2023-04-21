24c5e3b7ad4aed7494bbe2836c6ccb3d3bfc8621
hzk <huangzike@qixiaowei.net>
2023-04-21 17:12:32 +0800

    menu dml update

--- sql/update/v2023.4.2/dml/system_manage-dml-update.sql
+++ sql/update/v2023.4.2/dml/system_manage-dml-update.sql
@@ -4,3 +4,6 @@ SET NAMES utf8mb4;
 -- ----------------------------
 -- add of menu
 -- ----------------------------
+UPDATE menu SET sort=1 WHERE menu_id=10;
+UPDATE menu SET sort=3 WHERE menu_id=13;
+UPDATE menu SET sort=2 WHERE menu_id=14;
