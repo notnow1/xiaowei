c0511a9993529910167bd78c54c05f783de93a24
汤密持 <tangmichi@qixiaowei.net>
2023-04-24 16:59:05 +0800

    [项目名称：战略云微服务][提交人：汤密持][提交说明：bug修复]

--- qixiaowei-service/qixiaowei-system-manage/src/main/java/net/qixiaowei/system/manage/service/impl/basic/EmployeeServiceImpl.java
+++ qixiaowei-service/qixiaowei-system-manage/src/main/java/net/qixiaowei/system/manage/service/impl/basic/EmployeeServiceImpl.java
@@ -603,9 +603,7 @@ public class EmployeeServiceImpl implements IEmployeeService {
         //查询所有省市区
         List<RegionDTO> regionProvinceNameAndCityNameAndDistrictNames = regionMapper.selectRegionByProvinceNameAndCityNameAndDistrictName();
         //所有岗位
-        Post post = new Post();
-        post.setStatus(1);
-        List<PostDTO> postDTOS = postMapper.selectPostList(post);
+        List<PostDTO> postDTOS = postMapper.selectPostList(new Post());
         Department departmentExcel = new Department();
         departmentExcel.setStatus(1);
         //查询部门名称附加父级名称
