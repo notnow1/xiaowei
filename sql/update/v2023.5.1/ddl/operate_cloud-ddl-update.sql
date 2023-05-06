265fec98c05f9d03c230008828bae8c0da3d3cb5
hzk <huangzike@qixiaowei.net>
2023-05-06 10:27:23 +0800

    salary_item ddl add

--- sql/update/v2023.5.1/ddl/operate_cloud-ddl-update.sql
+++ sql/update/v2023.5.1/ddl/operate_cloud-ddl-update.sql
@@ -1,6 +1,7 @@
 USE `operate_cloud`;
 SET NAMES utf8mb4;
 -- ----------------------------
--- ddl add none
+-- ddl update salary_item
 -- ----------------------------
-
+ALTER TABLE `salary_item`
+ADD COLUMN status TINYINT UNSIGNED NOT NULL   COMMENT '状态:0失效;1生效' AFTER `sort`;
