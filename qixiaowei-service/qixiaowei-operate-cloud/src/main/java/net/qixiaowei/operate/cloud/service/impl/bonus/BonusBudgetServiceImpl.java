d2bb85ba36f47700b85280f540af4170dc83234e
汤密持 <tangmichi@qixiaowei.net>
2023-05-09 21:42:43 +0800

    [项目名称：战略云微服务][提交人：汤密持][提交说明：部门奖金预算]

--- qixiaowei-service/qixiaowei-operate-cloud/src/main/java/net/qixiaowei/operate/cloud/service/impl/bonus/BonusBudgetServiceImpl.java
+++ qixiaowei-service/qixiaowei-operate-cloud/src/main/java/net/qixiaowei/operate/cloud/service/impl/bonus/BonusBudgetServiceImpl.java
@@ -895,7 +895,7 @@ public class BonusBudgetServiceImpl implements IBonusBudgetService {
         //奖金占比基准值 公式=奖金包实际数÷奖金驱动因素实际数
         if (null != bonusProportionDrivingFactor && bonusProportionDrivingFactor.compareTo(new BigDecimal("0")) != 0 &&
                 null != bonusActualSum && bonusActualSum.compareTo(new BigDecimal("0")) != 0) {
-            bonusProportionStandard = bonusActualSum.divide(bonusProportionDrivingFactor.multiply(new BigDecimal("10000")), 10, BigDecimal.ROUND_HALF_UP).setScale(2, BigDecimal.ROUND_HALF_UP);
+            bonusProportionStandard = bonusActualSum.divide(bonusProportionDrivingFactor.multiply(new BigDecimal("10000")), 10, BigDecimal.ROUND_HALF_UP).multiply(new BigDecimal("100"));
             //奖金占比基准值(%)
             bonusBudgetParametersDTO.setBonusProportionStandard(bonusProportionStandard);
         }
@@ -1392,7 +1392,7 @@ public class BonusBudgetServiceImpl implements IBonusBudgetService {
                     //奖金占比基准值 公式=奖金包实际数÷奖金驱动因素实际数
                     if (null != bonusProportionDrivingFactor && bonusProportionDrivingFactor.compareTo(new BigDecimal("0")) != 0 &&
                             null != bonusActualSum && bonusActualSum.compareTo(new BigDecimal("0")) != 0) {
-                        bonusProportionStandard = bonusActualSum.divide(bonusProportionDrivingFactor, 10, BigDecimal.ROUND_HALF_UP).setScale(2, BigDecimal.ROUND_HALF_UP);
+                        bonusProportionStandard = bonusActualSum.divide(bonusProportionDrivingFactor.multiply(new BigDecimal("10000")), 10, BigDecimal.ROUND_HALF_UP).multiply(new BigDecimal("100"));
                         //奖金占比基准值(%)
                         bonusBudgetParametersDTO.setBonusProportionStandard(bonusProportionStandard);
                     }
