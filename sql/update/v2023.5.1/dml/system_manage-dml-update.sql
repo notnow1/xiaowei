7a3181c3bae75167870afb9180eb40808e685b6f
汤密持 <tangmichi@qixiaowei.net>
2023-05-18 11:52:27 +0800

    [项目名称：战略云微服务][提交人：汤密持][提交说明：部门变更]

--- sql/update/v2023.5.1/dml/system_manage-dml-update.sql
+++ sql/update/v2023.5.1/dml/system_manage-dml-update.sql
@@ -23,4 +23,4 @@ UPDATE menu SET menu_name='停用', update_time=sysdate() WHERE menu_id=183;
 UPDATE menu SET permission_code='system:manage:department:employee:remove', update_time=sysdate() WHERE menu_id=206;
 UPDATE menu SET permission_code='system:manage:department:employee:import', update_time=sysdate() WHERE menu_id=207;
 UPDATE menu SET permission_code='system:manage:department:employee:export', update_time=sysdate() WHERE menu_id=208;
-UPDATE menu SET `path`='ArticleWageEdit',component='manage/result/emolumentSetting/articleWage/components/edit', update_time=sysdate() WHERE menu_id=346;
+UPDATE menu SET menu_type=2,cache_flag=1,visible_flag=0,`path`='ArticleWageEdit',component='manage/result/emolumentSetting/articleWage/components/edit', update_time=sysdate() WHERE menu_id=346;
